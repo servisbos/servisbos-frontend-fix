a0f8a2fc76b8afef7f7aa82c82a526f0b132e182
Iwin Rahardjo <iwinrahardjo@yahoo.com>
2019-01-30 12:00:35 +0700

    adding jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
-    return (
-      
-    );
+    return <Header />;
   }
 }
 
